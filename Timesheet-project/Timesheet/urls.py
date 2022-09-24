95f2b91da4a39096b0bb54fbecfe8a90cc4285d9
Ravin D <ravin.d3104@outlook.com>
2022-09-24 16:33:20 +0530

    The Signup, Login and Logout Page has been created

--- Timesheet-project/Timesheet/urls.py
+++ Timesheet-project/Timesheet/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from times import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+
+    path('accounts/', include('accounts.urls')), 
 
 ]

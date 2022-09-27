a69ee2adcbe5ac87088fc7e68ca7f47bd1179c1c
Ravin D <ravin.d3104@outlook.com>
2022-09-27 22:29:20 +0530

    Modified the models.py file in times app and migrated it

--- Timesheet-project/times/models.py
+++ Timesheet-project/times/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Times(models.Model):
+    pub_date = models.DateTimeField()
+    description = models.TextField()
+    approved = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def limit(self):
+        return self.body[0:51]

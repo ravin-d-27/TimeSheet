a3861cb5af7c6d8420d5021b45d50370c3d57791
Ravin D <ravin.d3104@outlook.com>
2022-09-28 19:51:44 +0530

    Done With update.py, check why it is not getting registerd in admin

--- Timesheet-project/times/views.py
+++ Timesheet-project/times/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Times
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'times/home.html')
+
+@login_required(login_url = "/accounts/login")
+def update(request):
+        if request.method == 'POST':
+            if request.POST['description'] and request.POST['approval'] :
+                times = Times()
+                times.description = request.POST['description']
+                times.approved = request.POST['approval']
+
+                times.pub_date = timezone.datetime.now()
+                times.user = request.user
+
+                times.save()
+                return redirect("")
+            else:
+                return render(request,'times/update.html',{'error':'All Fields are required!'})
+
+        else:
+            return render(request,'times/update.html')

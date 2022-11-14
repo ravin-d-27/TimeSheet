63c10585a474e304c8cba7d63d95681a181b104f
Ravin D <ravin.d3107@outlook.com>
2022-11-14 09:01:50 +0530

    Change in DB and migrated all the data

--- Timesheet-project/Timesheet/settings.py
+++ Timesheet-project/Timesheet/settings.py
@@ -75,15 +75,12 @@ WSGI_APPLICATION = 'Timesheet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = { 'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'timesheet',
-'HOST': '127.0.0.1',
-'PORT': '3306',
-'USER': 'admin',
-'PASSWORD': 'admin1234',
-} }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
--- Timesheet-project/Timesheet/urls.py
+++ Timesheet-project/Timesheet/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from times import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -26,4 +27,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('update/', views.update, name='update'),
 
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
